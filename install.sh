dbed6549c5bf645107794506641367ee15af2e01
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-02-13 10:40:30 -0800

    Update scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+print_status() {
+  echo -ne " * $1\033[80G"
+}
+
+print_success() {
+  echo -ne "\033[74G[\033[32m OK \033[00m]\n"
+}
+
+print_fail() {
+  echo -ne "\033[74G[\033[31m NG \033[00m]\n"
+}
+
+print_status "Installing DOT Files"
 if [ ! -d ${HOME}/.dotfiles ] ; then
-  echo "Installing DOT Files"
   cd ${HOME} && git clone git@github.com:jbussdieker/dotfiles.git .dotfiles
+  print_success
+
   cd ${HOME}/.dotfiles && ./setup.sh
 else
-  echo "DOT Files already installed"
+  print_fail
+  echo "DOT Files already installed!"
 fi
--- setup.sh
+++ setup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-echo "Setting up bash dot files"
-ln -s -f ~/.dotfiles/bash/profile ~/.profile
-ln -s -f ~/.dotfiles/bash/profile ~/.profile
+print_status() {
+  echo -ne " * $1\033[80G"
+}
 
-echo "Setting up vim dot files"
+print_success() {
+  echo -ne "\033[74G[\033[32m OK \033[00m]\n"
+}
+
+print_fail() {
+  echo -ne "\033[74G[\033[31m NG \033[00m]\n"
+}
+
+print_status "Setting up bash dot files"
+ln -s -f ~/.dotfiles/bash/profile ~/.profile
+ln -s -f ~/.dotfiles/bash/profile ~/.profile
+print_success
+
+print_status "Setting up vim dot files"
 ln -s -f ~/.dotfiles/vim/vimrc ~/.vimrc
+print_success
